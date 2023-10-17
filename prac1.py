23e80073c2bc05bbd6c8e19b43c55c07e1840ecb
Prithvi Sharan <prithvisharan21@gmail.com>
2023-10-17 22:07:04 +0530

    Hello World | Variables and Data Types

---
+++ prac1.py
@@ -0,0 +1,17 @@
+name = "Prithvi Sharan"
+age = 22
+cities = ['Bangalore','Ahmedabad',66]
+dict = {'prithvi':age+12,'prithvi sharan':age}
+bol =  True
+bol1= False
+
+print('Hello World!')
+
+print('Hello World!')
+
+print('Hello World!')
+
+print(name)
+print(dict['prithvi'])
+
+print(age)
